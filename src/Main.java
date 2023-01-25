2623904004daa4a05f79dee73ba58a590d8feba6
DarynaHorobei97 <daryna_horobei1@ukr.net>
2023-01-25 13:53:18 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+
+    static int a = 6;
+
+    static void showText(String word) {
+        System.out.println(word);
+    }
+
+    static void showText(int number) {
+        System.out.println(number);
+    }
+
+    static void showText(String letter, int number) {
+        System.out.println(letter);
+    }
+
+    static void showText(int number, String letter) {
+        System.out.println(letter);
+    }
+
+
+    public static void main(String[] args) {
+
+        showText(5, "fghjk");
+
+    }
+
+}
